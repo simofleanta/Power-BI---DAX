8b67efefa9d8106e01e561d3882ddb6fe6896da4
simofleanta <simo.fleanta@gmail.com>
2021-08-27 12:55:26 +0300

    Create dax1.m

---
+++ dax1.m
@@ -0,0 +1,3 @@
+Autumn costs in Berlin = SUMX(energy_startup, IF(energy_startup[Season]="autumn" && energy_startup[Client_Region]="Berlin", energy_startup[Costs],energy_startup[Client_Room_household]))
+
+Autumn in berlin = SUMX(energy_startup, IF(energy_startup[Season]="autumn" && energy_startup[Client_Region]="Berlin", energy_startup[Sales_rev],energy_startup[Client_Room_household]))
